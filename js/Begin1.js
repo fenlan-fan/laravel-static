64c0e8ae06e9b4b93c38b704ef75aa948114b342
fanzhonghao <1412328318@qq.com>
2017-08-18 16:14:19 +0800

    Begin

---
+++ js/Begin1.js
@@ -0,0 +1,22 @@
+function start() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "none";
+    var us = document.getElementById("us");
+    us.style.display = "none";
+}
+function shop() {
+    var menu =document.getElementById("menu");
+    menu.style.display = "block";
+}
+function shop1() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
+function aboutUs() {
+    var us = document.getElementById("us");
+    us.style.display = "block";
+}
+function aboutUs1() {
+    var us = document.getElementById("us");
+    us.style.display = "none";
+}

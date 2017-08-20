539d13c88bf7b54c267aea71f7948aff164c2a22
fanzhonghao <1412328318@qq.com>
2017-08-20 21:24:49 +0800

    admAdd.js

--- js/admAdd.js
+++ js/admAdd.js
@@ -12,5 +12,38 @@ function cancel() {
     }
 }
 function submit() {
-    return confirm("确认提交？");
+    if(!confirm("确认提交？")) return false;
+    document.getElementById("bookName").style.borderColor = "";
+    document.getElementById("author").style.borderColor = "";
+    document.getElementById("price").style.borderColor = "";
+    document.getElementById("aboutAuthor").style.borderColor = "";
+    document.getElementById("aboutBook").style.borderColor = "";
+    var a = true;
+    if (document.getElementById("aboutBook").value == ""){
+        document.getElementById("aboutBook").focus();
+        document.getElementById("aboutBook").style.borderColor = "red";
+        a = false;
+    }
+    if (document.getElementById("aboutAuthor").value == ""){
+        document.getElementById("aboutAuthor").focus();
+        document.getElementById("aboutAuthor").style.borderColor = "red";
+        a = false;
+    }
+    if (document.getElementById("price").value == ""){
+        document.getElementById("price").focus();
+        document.getElementById("price").style.borderColor = "red";
+        a = false;
+    }
+    if (document.getElementById("author").value == ""){
+        document.getElementById("author").focus();
+        document.getElementById("author").style.borderColor = "red";
+        a = false;
+    }
+    if (document.getElementById("bookName").value == ""){
+        document.getElementById("bookName").focus();
+        document.getElementById("bookName").style.borderColor = "red";
+        a = false;
+    }
+
+    return a;
 }
